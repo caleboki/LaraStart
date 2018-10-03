a9c71043ef160b49d2304adf4e75712ec819dbbf
Caleb Oki <caleboki@gmail.com>
2018-10-03 11:27:53 +0100

    second commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,14 +9,29 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile', component: require('./components/Profile.vue') },
+    { path: '/users', component: require('./components/Users.vue') }
+]
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+const router = new VueRouter({
+    mode: 'history',
+    routes // short for `routes: routes`
+})
+
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
